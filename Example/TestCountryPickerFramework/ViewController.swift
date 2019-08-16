04a2634efae2cb71a38a7d94cb5dfbb52b7d2943
Sharkes Monken <sharkesm@gmail.com>
2019-08-16 23:21:47 +0800

    Refactor ViewController.swift

--- Example/TestCountryPickerFramework/ViewController.swift
+++ Example/TestCountryPickerFramework/ViewController.swift
@@ -14,11 +14,10 @@ class ViewController: UIViewController {
     // MARK: - IBOutlet
     @IBOutlet weak var countryCodeButton: UIButton!
     @IBOutlet weak var countryImageView: UIImageView!
-    let contryPickerController = CountryPickerController()
-
     @IBOutlet weak var showDialingCodeSwitch: UISwitch!
     @IBOutlet weak var showWithSectionsSwitch: UISwitch!
     @IBOutlet weak var showCountryFlagSwitch: UISwitch!
+    
     // MARK: - Func
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,8 +43,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func countryCodeButtonClicked(_ sender: UIButton) {
-        switch showWithSectionsSwitch.isOn {
+        presentCountryPickerScene(withSelectionControlEnabled: showWithSectionsSwitch.isOn)
+    }
+}
+
+
+private extension ViewController {
+    
+    /// Dynamically presents country picker scene with an option of including `Selection Control`.
+    ///
+    /// By default, invoking this function without defining `selectionControlEnabled` parameter. Its set to `True`
+    /// unless otherwise and the `Selection Control` will be included into the list view.
+    ///
+    /// - Parameter selectionControlEnabled: Section Control State. By default its set to `True` unless otherwise.
+    
+    func presentCountryPickerScene(withSelectionControlEnabled selectionControlEnabled: Bool = true) {
+        switch selectionControlEnabled {
         case true:
+            // Present country picker with `Section Control` enabled
             let countryController = CountryPickerWithSectionViewController.presentController(on: self) { [weak self] (country: Country) in
                 
                 guard let self = self else { return }
@@ -60,11 +75,16 @@ class ViewController: UIViewController {
             countryController.isHideFlagImage = !showCountryFlagSwitch.isOn
             countryController.isHideDiallingCode = !showDialingCodeSwitch.isOn
         case false:
-            let countryController = CountryPickerController.presentController(on: self) { (country: Country) in
-                self.countryImageView.image = country.flag
+            // Present country picker without `Section Control` enabled
+            let countryController = CountryPickerController.presentController(on: self) { [weak self] (country: Country) in
+                
+                guard let self = self else { return }
+                
                 self.countryImageView.isHidden = false
+                self.countryImageView.image = country.flag
                 self.countryCodeButton.setTitle(country.dialingCode, for: .normal)
             }
+            
             countryController.detailColor = UIColor.blue
             countryController.labelColor = UIColor.green
             countryController.isHideFlagImage = !showCountryFlagSwitch.isOn
