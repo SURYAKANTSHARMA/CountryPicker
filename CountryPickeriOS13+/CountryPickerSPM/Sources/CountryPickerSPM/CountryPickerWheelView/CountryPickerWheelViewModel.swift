4f54a913c6338f120773fd284ba04dffcecca60a
Ankush Bhatia <ankushbhatia1347@gmail.com>
2023-08-02 01:44:54 +0530

    Creates CountryPickerWheelViewModel

---
+++ CountryPickeriOS13+/CountryPickerSPM/Sources/CountryPickerSPM/CountryPickerWheelView/CountryPickerWheelViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by ANKUSH BHATIA on 8/2/23.
+//
+
+import Foundation
+
+final class CountryPickerWheelViewModel: ObservableObject {
+    
+    @Published var countries: [Country]
+    @Published var selected: Int
+    
+    init(countries: [Country], selected: Int) {
+        self.countries = countries
+        self.selected = selected
+    }
+}
